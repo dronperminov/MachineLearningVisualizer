3cb1c31c09ff88bd04e8ce697431196a0f0ba7df
dronperminov <dronperminov@gmail.com>
2021-09-28 23:30:58 +0300

    add non derivative version of loss function

--- js/neural-network/loss.js
+++ js/neural-network/loss.js
@@ -1,9 +1,24 @@
-function MSE(y, t, deltas) {
+function LossFunction() {
+
+}
+
+LossFunction.prototype.EvaluateDeltas = function(y, t, deltas) {
     let loss = 0
 
     for (let i = 0; i < y.length; i++) {
         let e = y[i] - t[i]
         deltas[i] = 2*e
+        loss += e*e
+    }
+
+    return loss
+}
+
+LossFunction.prototype.Evaluate = function(y, t) {
+    let loss = 0
+
+    for (let i = 0; i < y.length; i++) {
+        let e = y[i] - t[i]
         loss += e*e
     }
 
